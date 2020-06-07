ee5e5e7fb2f5a81513e7a1d6a2bd72a26b139f22
Kremenevskiy <vkremenevskiy@gmail.com>
2020-06-07 11:35:54 +0300

    Started lab5

---
+++ LAB_5/Lab5/doublylist.cpp
@@ -0,0 +1,6 @@
+#include "doublylist.h"
+
+DoublyList::DoublyList()
+{
+
+}
---
+++ LAB_5/Lab5/doublylist.h
@@ -0,0 +1,11 @@
+#ifndef DOUBLYLIST_H
+#define DOUBLYLIST_H
+
+
+class DoublyList
+{
+public:
+	DoublyList();
+};
+
+#endif // DOUBLYLIST_H
