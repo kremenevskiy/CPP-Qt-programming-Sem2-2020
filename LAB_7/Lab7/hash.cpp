fbdac2b1c3dfc8a6c4cdc46f55cfe8e9a65823d5
Kremenevskiy <vkremenevskiy@gmail.com>
2020-06-10 19:31:07 +0300

    HashTable implementation

--- LAB_7/Lab7/hash.cpp
+++ LAB_7/Lab7/hash.cpp
@@ -1,6 +1,62 @@
 #include "hash.h"
 
+
 Hash::Hash()
 {
+	for(int i = 0; i < hashTableSize; ++i){
+		HashTable[i] = new Item;
+		HashTable[i]->value = "NULL";
+		HashTable[i]->next = nullptr;
 
+	}
+
+}
+
+
+int Hash::hash(QString key){
+	int hash = 0;
+	for(int i = 0; i < key.length(); ++i){
+		hash += (int) key[0].toLatin1();
+	}
+
+	hash %= hashTableSize;
+	return hash;
+
+}
+
+
+void Hash::addItem(QString value){
+	int index = hash(value);
+
+	if(!HashTable[index]->next){
+		HashTable[index]->value = value;
+	}
+	else{
+		Item *current = HashTable[index];
+		Item *newItem = new Item;
+		newItem->value = value;
+		newItem->next = nullptr;
+
+		while(!current->next){
+			current = current->next;
+		}
+
+		current->next = newItem;
+
+
+	}
+}
+
+
+int Hash::numberOfItemsInIndex(int index){
+	int count = 0;
+
+	Item* current = HashTable[index];
+
+	while(current->next){
+		count ++;
+		current = current->next;
+	}
+
+	return count;
 }
--- LAB_7/Lab7/hash.h
+++ LAB_7/Lab7/hash.h
@@ -1,11 +1,35 @@
 #ifndef HASH_H
 #define HASH_H
 
+#include <QString>
+#include <QDebug>
+
+
 
 class Hash
 {
 public:
+	struct Item{
+		QString value;
+		Item *next;
+	};
+
 	Hash();
+	static const int hashTableSize = 10;
+
+	Item* HashTable[hashTableSize];
+
+	int hash(QString key);
+	void addItem(QString value);
+
+	int numberOfItemsInIndex(int index);
+	Item* findItem(	 )
+
+
+
+
+private:
+
 };
 
 #endif // HASH_H
